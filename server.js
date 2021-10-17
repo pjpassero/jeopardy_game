0345e42b5290c6febbc57b1023cfd1591f15bfc7
Developer1545 <59211806+Developer1545@users.noreply.github.com>
2021-10-17 18:55:09 -0400

    Added Server File Via Upload

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+let port = 3000;
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+
+
+app.listen(port, function(){
+    console.log(`Server is running on ${port}`);
+})
+
+
+
